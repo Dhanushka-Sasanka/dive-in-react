65a5110aa2d505e7dfc991e6496db85dc32f5eea
Dhanushka-Sasanka <dhanushkasasankapayza@gmail.com>
2024-02-03 01:47:01 +0530

    data passing via props

---
+++ react-js/005 01-starting-setup/01-starting-setup/src/App.js
@@ -0,0 +1,56 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+
+  const expensesList = [
+    { expenceDate: new Date(2024, 2, 1), expenseTitel: "Food Expense", expenseAmount: 250021.21 },
+    { expenceDate: new Date(2024, 2, 2), expenseTitel: "Car Expense", expenseAmount: 32000.23 },
+    { expenceDate: new Date(2024, 2, 3), expenseTitel: "Travel Expense", expenseAmount: 12001.25 },
+    { expenceDate: new Date(2024, 2, 4), expenseTitel: "Medicine Expense", expenseAmount: 54210.52 },
+    { expenceDate: new Date(2024, 2, 5), expenseTitel: "Clothes Expense", expenseAmount: 45000.26 },
+  ];
+
+
+  return (
+    <div>
+      <h2>Buget Calculator</h2>
+      {/* can also pass complete object */}
+      {/* <ExpenseItem
+        expense={expensesList[0]}
+      /> */}
+
+      
+      <ExpenseItem
+        expenseDate={expensesList[0].expenceDate}
+        expenseTitel={expensesList[0].expenseTitel}
+        expenseAmount={expensesList[0].expenseAmount}
+      />
+
+      <ExpenseItem
+        expenseDate={expensesList[1].expenceDate}
+        expenseTitel={expensesList[1].expenseTitel}
+        expenseAmount={expensesList[1].expenseAmount}
+      />
+
+      <ExpenseItem
+        expenseDate={expensesList[2].expenceDate}
+        expenseTitel={expensesList[2].expenseTitel}
+        expenseAmount={expensesList[2].expenseAmount}
+      />
+
+      <ExpenseItem
+        expenseDate={expensesList[3].expenceDate}
+        expenseTitel={expensesList[3].expenseTitel}
+        expenseAmount={expensesList[3].expenseAmount}
+      />
+
+      <ExpenseItem
+        expenseDate={expensesList[4].expenceDate}
+        expenseTitel={expensesList[4].expenseTitel}
+        expenseAmount={expensesList[4].expenseAmount}
+      />
+    </div>
+  );
+}
+
+export default App;
